f8bd13fedb351a7634479860a65555f1a2692e66
Diksha Singh <dikshasingh25202520@gmail.com>
2025-07-17 14:01:20 +0530

    new changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -19,7 +19,13 @@ const io = connectToSocket(server);
 
 
 app.set("port", (process.env.PORT || 5000))
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://videocall-codealpha.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
